148fb24d3dd1b536e98d4e2b2ff11bb032cc5773
Rakesh Biswal <rb2306114@gmail.com>
2026-04-15 14:19:39 +0530

    chhh

--- backend/server.js
+++ backend/server.js
@@ -22,8 +22,24 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(cors());
+
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://bss-power.vercel.app'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('CORS not allowed'));
+    }
+  },
+  credentials: true
+}));
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
